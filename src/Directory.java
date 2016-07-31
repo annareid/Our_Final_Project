2191d379c9a61a08ef5325d31ab6b6edd048badd
Pariahakimi <Paria@Parias-MBP.netgear.com>
2016-07-31 10:35:29 -0700

    added three helper method

--- src/Directory.java
+++ src/Directory.java
@@ -18,12 +18,11 @@ public class Directory {
         root.getChars( 0, fsizes[0], fnames[0], 0 ); // fnames[0] includes "/"
     }
 
+    // assumes data[] received directory information from disk
+    // initializes the Directory instance with this data[]
+    // data[] starts with file sizes. File names are stored after the file sizes.
     public void bytes2directory( byte data[] ) {
-        // assumes data[] received directory information from disk
-        // initializes the Directory instance with this data[]
 
-
-        // Assumption:  data[] starts with file sizes. File names are stored after the file sizes.
         int byteOffset = 0;
         for (int i = 0; i < fsizes.length; i++, byteOffset +=4){
             fsizes[i] = SysLib.bytes2int(data, byteOffset);
@@ -34,13 +33,11 @@ public class Directory {
         }
     }
 
+    // converts and return Directory information into a plain byte array
+    // this byte array will be written back to disk
+    // note: only meaningfull directory information should be converted into bytes.
+    // Assumption: the meaningful information is file size & file name.
     public byte[] directory2bytes( ) {
-        // converts and return Directory information into a plain byte array
-        // this byte array will be written back to disk
-        // note: only meaningfull directory information should be converted
-        // into bytes.
-
-        // Assumption the meaningful information is file size & file name.
 
         byte[] dirBytes = new byte[(fsizes.length * 4) + (fnames.length *maxChars *2)];
         int byteOffset = 0;
@@ -48,7 +45,7 @@ public class Directory {
         {
             SysLib.int2bytes(fsizes[i], dirBytes, byteOffset);
         }
-        for(int i = 0; i <fsizes.length; i++)       //stores file names into array dirBytes
+        for(int i = 0; i <fsizes.length; i++)                     //stores file names into array dirBytes
         {
             for(int j = 0; j < maxChars * 2; j++ )
             {
@@ -59,23 +56,47 @@ public class Directory {
         return dirBytes;
     }
 
+    // filename is the one of a file to be created.
+    // allocates a new inode number for this filename
     public short ialloc( String filename ) {
-        // filename is the one of a file to be created.
-        // allocates a new inode number for this filename
 
-        return 0;
-    }
+            for(short i = 1; i < fsizes.length; i++)
+            {
+                if(fsizes[i] == 0)              //we use the first empty spot
+                {
+                    fsizes[i] = Math.min(filename.length(), maxChars);
+                    filename.getChars(0, fsizes[i], fnames[i], 0);
+                    return i;
+                }
+            }
+            return -1;
+        }
 
+    // deallocates this inumber (inode number)
+    // the corresponding file will be deleted.
     public boolean ifree( short iNumber ) {
-        // deallocates this inumber (inode number)
-        // the corresponding file will be deleted.
 
-        return true;
-    }
+            if(fsizes[iNumber] > 0)         //checks to see if it is valid
+            {
+                fsizes[iNumber] = 0;        //set it to zero
+                return true;
+            }
+            else                          //if it is not found
+                return false;
+        }
 
+    // returns the inumber corresponding to this filename
     public short namei( String filename ) {
-        // returns the inumber corresponding to this filename
 
-        return 0;
+        for(short i = 0; i < fsizes.length; i++)
+        {
+            if(this.fsizes[i] == filename.length())
+            {
+                String name = new String(fnames[i], 0, fsizes[i]);
+                if(filename.equals(name))
+                    return i;
+            }
+        }
+        return -1;
     }
 }
