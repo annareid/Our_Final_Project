4c0a80ecf13492843fed2a977d7ca0e527b19cd0
Anna Reid <anna.koldunova@gmail.com>
2016-08-06 15:46:13 -0700

    Add comments
    Methode completed

--- src/Inode.java
+++ src/Inode.java
@@ -2,16 +2,16 @@
  * Created by Michael on 7/23/2015.
  */
 public class Inode {
-    private final static int iNodeSize = 32;       // fix to 32 bytes
-    private final static int directSize = 11;      // # direct pointers
+    private final static int iNodeSize = 32;                    // fix to 32 bytes
+    private final static int directSize = 11;                   // # direct pointers
 
-    public int length;                             // file size in bytes
-    public short count;                            // # file-table entries pointing to this
-    public short flag;                             // 0 = unused, 1 = used, ...
-    public short direct[] = new short[directSize]; // direct pointers
-    public short indirect;                         // a indirect pointer
+    public int length;                                          // file size in bytes
+    public short count;                                         // # file-table entries pointing to this
+    public short flag;                                          // 0 = unused, 1 = used, ...
+    public short direct[] = new short[directSize];              // direct pointers
+    public short indirect;                                      // a indirect pointer
 
-    public Inode( ) {                                     // a default constructor
+    public Inode( ) {                                           // a default constructor
         length = 0;
         count = 0;
         flag = 1;
@@ -20,11 +20,13 @@ public class Inode {
         indirect = -1;
     }
 
-    public Inode( short iNumber ) {                       // retrieving inode from disk
-        byte[] inodeBytes = new byte[Disk.blockSize];
-        SysLib.rawread(getBlock(iNumber), inodeBytes);
+    public Inode( short iNumber ) {                             // retrieving inode from disk
+        byte[] inodeBytes = new byte[Disk.blockSize];           //buffer array
+        SysLib.rawread(getBlock(iNumber), inodeBytes);          // read a block to the buffer array
 
-        int byteOffSet = getByteOffsetInBlock(iNumber);
+        int byteOffSet = getByteOffsetInBlock(iNumber);         //offset in block for this inode
+
+        // write length, count, flag, direct, indirect into the buffer array
         length = SysLib.bytes2int(inodeBytes, byteOffSet + 0);
         count = SysLib.bytes2short(inodeBytes, byteOffSet + 4);
         flag = SysLib.bytes2short(inodeBytes, byteOffSet + 6);
@@ -35,8 +37,10 @@ public class Inode {
         indirect = SysLib.bytes2short(inodeBytes, byteOffSet + 30);
     }
 
-    public int toDisk( short iNumber ) {                  // save to disk as the i-th inode
-        byte[] inodeBytes = new byte[iNodeSize];
+    public int toDisk( short iNumber ) {                        // save to disk as the i-th inode
+        byte[] inodeBytes = new byte[iNodeSize];                //buffer array
+
+        // write length, count, flag, direct, indirect into the buffer array
         SysLib.int2bytes(length, inodeBytes, 0);
         SysLib.short2bytes(count, inodeBytes, 4);
         SysLib.short2bytes(flag, inodeBytes, 6);
@@ -46,22 +50,27 @@ public class Inode {
         }
         SysLib.short2bytes(indirect, inodeBytes, 30);
 
+
         byte[] blockBytes = new byte[Disk.blockSize];
         int block = getBlock(iNumber);
         SysLib.rawread(block, blockBytes);
 
-        int byteOffset = getByteOffsetInBlock(iNumber);
+
+        int byteOffset = getByteOffsetInBlock(iNumber);          //get the offset
         for (int i = 0; i < inodeBytes.length; ++i) {
             blockBytes[byteOffset + i] = inodeBytes[i];
         }
 
+        //write the block back to the disk
         return SysLib.rawwrite(block, blockBytes);
     }
 
+    // Based on the inode number passed to it, this method finds the right block for an inode to be stored.
     public int getBlock( short iNumber ) {
         return 1 + iNumber / SuperBlock.inodesPerBlock;
     }
 
+    //Based on the inode number passed to it, this method finds the right byte offset for the inode.
     public int getByteOffsetInBlock( short iNumber ) {
         return iNumber % SuperBlock.inodesPerBlock * iNodeSize;
     }
