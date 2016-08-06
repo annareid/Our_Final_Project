abbe4f50d8daea0f15c087c910f4e0d578964c67
Anna Reid <anna.koldunova@gmail.com>
2016-08-06 15:36:04 -0700

    Complete SuperBlock
    Added getFreeBlock and returnBlock.

--- src/SuperBlock.java
+++ src/SuperBlock.java
@@ -1,8 +1,6 @@
+
 import java.util.ArrayList;
 
-/**
- * Created by Michael on 7/23/2015.
- */
 class SuperBlock {
     public static final int inodesPerBlock = 16;
     private final int defaultInodeBlocks = 64;
@@ -21,15 +19,15 @@ class SuperBlock {
         if (totalBlocks == diskSize && totalInodes > 0 && freeList >= 2)
             //disk contents are valid
             return;
-        else{
+        else {
             //need to format disk
             totalBlocks = diskSize;
             format(defaultInodeBlocks);
         }
     }
 
-    public void format(int inodeBlocks) // formats the disk, creates superblock & specified # of inodes
-    {
+    public void format(int inodeBlocks){ // formats the disk, creates superblock & specified # of inodes
+
         totalInodes = inodeBlocks * inodesPerBlock;
         freeList = 1 + inodeBlocks; // start at 1 because the superblock occupies block 0
 
@@ -44,16 +42,29 @@ class SuperBlock {
             currentInode.toDisk(i);
         }
 
+        // set each free block to point at the next free block
+        byte[] freeListPointer = new byte[Disk.blockSize];
+        int freeBlock = freeList;
+        for(int nextFreeBlockPointer = freeList + 1; nextFreeBlockPointer < 1000; nextFreeBlockPointer++ ){
+
+            SysLib.int2bytes(nextFreeBlockPointer, freeListPointer, 0);
+            SysLib.rawwrite(freeBlock, freeListPointer);
+            freeBlock ++;
+        }
+
+        //set the last free block to point at -1
+        for(int i = 0; i < freeListPointer.length; i++ ){
+            freeListPointer[i] = 0;
+        }
+        SysLib.int2bytes(-1, freeListPointer, 0);
+        SysLib.rawwrite(freeBlock, freeListPointer);
         SysLib.sync();
     }
 
-    //Other SuperBlock Methods from Additional Materials
-
     // Writes back totalBlocks, inodeBlocks, and freeList to disk
-    // sync()
-    void sync()
+    public void sync()
     {
-        byte[] block = new byte[512];                    //Created a buffer
+        byte[] block = new byte[512];                   //Created a buffer
         SysLib.int2bytes(totalBlocks, block, 0);        //Load total blocks into the buffer as bytes
         SysLib.int2bytes(totalInodes, block, 4);        //Load total Inodes into buffer as bytes
         SysLib.int2bytes(freeList, block, 8);           //Load freelist into buffer as bytes
@@ -61,11 +72,51 @@ class SuperBlock {
         SysLib.rawwrite(0, block);                      //Write buffer to disk
 
     }
+
     //Dequeues the top block from the free list
-    // getFreeBlock()
+    public int getFreeBlock(){
+        int freeBlock = freeList;                       //freeBlock number
+        int nextFreeBlock;                              //pointer ot the next free block
+
+        byte[] block = new byte[Disk.blockSize];
+        SysLib.rawread(freeList, block);                //read the block from the disk
+        nextFreeBlock = SysLib.bytes2int(block, 0);     //get the pointer to the next free block
+
+        freeList = nextFreeBlock;                       //update the freeList pointer
+
+        sync();
+        return freeBlock;
+    }
 
     //Enqueue a given block to the end of the free list
-    //returnBlock(int blockNumber)
+    public void returnBlock(int blockNumber){
+        byte[] block = new byte[Disk.blockSize];
+        int nextFreeBlock = 0;                          //pointer to the next free block
+        int nextBlockToRead = freeList;                 //the block we are currently reading
+        int previousBlockToRead = 0;                    //the previous block we read
+
+        // find a block that points at -1
+        while (nextFreeBlock != -1){
+            SysLib.rawread(nextBlockToRead, block);
+            nextFreeBlock = SysLib.bytes2int(block, 0);
+            previousBlockToRead = nextBlockToRead;      //to keep track of the previous block we read
+            nextBlockToRead = nextFreeBlock;
+        }
+
+        nextFreeBlock = blockNumber;
+
+        //update the previous block's next pointer and write the changes to disk
+        byte[] blockToReturn = new byte[Disk.blockSize];
+        SysLib.int2bytes(nextFreeBlock, blockToReturn, 0);
+        SysLib.rawwrite(previousBlockToRead, blockToReturn);
+
+        // set the returned block's next pointer to -1 and write the changes to disk
+        for(int i = 0; i < blockToReturn.length; i++ ){
+            blockToReturn[i] = 0;
+        }
+        SysLib.int2bytes(-1, blockToReturn, 0);
+        SysLib.rawwrite(nextFreeBlock, blockToReturn);
+    }
 
 
 
